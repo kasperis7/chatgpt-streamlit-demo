51a06822da35b1d3c93d79305f16112bfe25de4b
cyanide <hmwoo@zju.edu.cn>
2023-02-04 18:07:35 +0800

    init

---
+++ laohu.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from revChatGPT.Official import Chatbot
+
+api_key = st.secrets("OPENAI_API_KEY")
+chatbot = Chatbot(api_key)
+
+def ask():
+	prompt = st.text_input("请输入你要问老胡的问题")
+	return prompt
+
+st.header("ChatGPT老胡模拟器")
+prompt = ask()
+if prompt:
+	chatbot.ask("你要像胡锡进那样回答我的问题。开头加上“老胡认为”，中间加上“但是，老胡还认为”，并且后半段的回答要用来反驳前半段的回答。")
+	output = chatbot.ask(prompt)["choices"][0]["text"]
+	st.write(output)
+	chatbot.reset()
